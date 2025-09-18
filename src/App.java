f294647b835af1a3ccbd39638d378c9e286162c8
Ignacio martinez <ignaciomartinezrodriguez5@gmail.com>
2025-09-18 11:24:32 +0200

    Primera versión

---
+++ src/App.java
@@ -0,0 +1,14 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hola, Ignacio MR!");
+        System.out.println("Vamos a aprender java");
+        int edad;
+        edad = 18;
+        boolean mayorDeEdad;
+        mayorDeEdad = edad>= 18;
+        double importe = 123.56;
+        System.out.println("Tengo "+edad+" años y "+importe+" euros en la cuenta");
+        System.out.println("¿Soy mayor de edad? "+mayorDeEdad);
+    }
+}
+ 
---
+++ src/MayorDeEdad.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class MayorDeEdad {
+ public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Cual es tu nombre?");
+        String nombre = sc.nextLine();
+        System.out.println("Cuantos años tienes?");
+        int edad = sc.nextInt();
+        final int MAYORYA_DE_EDAD = 18;
+        boolean mayorDeEdad = edad>=MAYORYA_DE_EDAD;
+        System.out.println(nombre+", eres mayor de edad? "+mayorDeEdad);
+     }
+ }
