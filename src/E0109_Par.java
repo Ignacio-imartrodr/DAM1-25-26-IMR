f2d7d1d0eaaa3a3836f3fff44b98c4987ee92d34
Ignacio imartrodr <ignaciomartinezrodriguez5@gmail.com>
2025-09-19 12:57:53 +0200

    Corrección 3Par

--- src/E0109_Par.java
+++ src/E0109_Par.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 /** 
- * Ejercicio EO109.java: Recibe un numero entero y responde si es par o no mediante un boolean (verdadero o falso)
+ * E0109_Par. Programa que pida un número entero al usuario y que indique si es par mediante un literal booleano (true o false).
  */
 
 /** 
@@ -8,17 +8,21 @@ import java.util.Scanner;
  */
 public class E0109_Par {
     public static void main(String[] args) throws Exception {
-
+        
+        //Abrimo Variables
         int num;
         boolean par;
-
+        
+        //Recogemos datos
         Scanner sc = new Scanner(System.in);
         System.out.print("Dame un número: ");
         num = sc.nextInt();
         sc.close();
         
-        par = num/2==(num/2)-(num%2);
-        
+        //Operamos datos
+        par = num/2 == (num/2) - (num%2);
+
+        //Mostramos resultados
         if (par==true) {
             System.out.println("Ese número es par");
         } else {
