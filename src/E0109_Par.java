35c0ad8ecf43fcbe099a0e9aa40da55a5ef77eb5
Ignacio imartrodr <ignaciomartinezrodriguez5@gmail.com>
2025-09-19 12:46:30 +0200

    Arreglo 2Par

--- src/E0109_Par.java
+++ src/E0109_Par.java
@@ -1,12 +1,16 @@
 import java.util.Scanner;
 /** 
+ * Ejercicio EO109.java: Recibe un numero entero y responde si es par o no mediante un boolean (verdadero o falso)
+ */
+
+/** 
  * @Author Ignacio MR
  */
 public class E0109_Par {
     public static void main(String[] args) throws Exception {
 
-        boolean par;
         int num;
+        boolean par;
 
         Scanner sc = new Scanner(System.in);
         System.out.print("Dame un número: ");
@@ -14,6 +18,7 @@ public class E0109_Par {
         sc.close();
         
         par = num/2==(num/2)-(num%2);
+        
         if (par==true) {
             System.out.println("Ese número es par");
         } else {
