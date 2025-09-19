d557e668e3ff7e05ef4cff3776b10761d9db91a5
Ignacio imartrodr <ignaciomartinezrodriguez5@gmail.com>
2025-09-19 12:42:24 +0200

    Arreglo Par

--- src/E0109_Par.java
+++ src/E0109_Par.java
@@ -4,15 +4,20 @@ import java.util.Scanner;
  */
 public class E0109_Par {
     public static void main(String[] args) throws Exception {
+
+        boolean par;
+        int num;
+
         Scanner sc = new Scanner(System.in);
-        System.out.print("Dame un numero: ");
-        int num = sc.nextInt();
+        System.out.print("Dame un número: ");
+        num = sc.nextInt();
         sc.close();
-        boolean par = num/2==(num/2)-(num%2);
+        
+        par = num/2==(num/2)-(num%2);
         if (par==true) {
-            System.out.println("Ese numero es par");
+            System.out.println("Ese número es par");
         } else {
-            System.out.println("Ese numero NO es par");
+            System.out.println("Ese número NO es par");
         }
      }
 }
