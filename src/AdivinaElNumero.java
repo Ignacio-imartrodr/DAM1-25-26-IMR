3ac45598f760d248d9ef84d08850a3909f5083f0
Ignacio imartrodr <ignaciomartinezrodriguez5@gmail.com>
2025-09-24 10:42:02 +0200

    Gambling

---
+++ src/AdivinaElNumero.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+/** 
+* 
+*/
+
+/** 
+* @Author Ignacio MR
+*/
+public class AdivinaElNumero {
+      public static void main(String[] args) throws Exception {
+
+            int num;
+      
+            
+            
+            System.out.print("acierta un numero del 0 al 10: ");
+            num = Math.round(Math.random()*10);
+            //double ad = num - num % 1;
+            System.out.println(num);
+            
+            /**while (res != ad) {
+                  System.out.println("Fallaste");
+                  System.out.print("acierta un numero del 0 al 10: ");
+                  Scanner sc = new Scanner(System.in);
+                  int res = sc.nextInt();
+                  }
+            *
+            */
+            
+
+
+
+      }
+}
