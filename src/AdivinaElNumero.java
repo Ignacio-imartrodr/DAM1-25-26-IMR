5fdefbf89cfa8d3a923209d018715710bc4630b2
Ignacio imartrodr <ignaciomartinezrodriguez5@gmail.com>
2025-09-24 12:56:02 +0200

    Gambling perfect

--- src/AdivinaElNumero.java
+++ src/AdivinaElNumero.java
@@ -10,24 +10,26 @@ import java.util.Scanner;
 public class AdivinaElNumero {
       public static void main(String[] args) throws Exception {
 
-            int num;
-      
+            long num;
+            int res;
+            String mayor;
+
+            num = Math.round(Math.random()*100);
+            System.out.print("Acierta un numero del 0 al 100: ");
+            Scanner sc = new Scanner(System.in);
+            res = sc.nextInt();
             
             
-            System.out.print("acierta un numero del 0 al 10: ");
-            num = Math.round(Math.random()*10);
-            //double ad = num - num % 1;
-            System.out.println(num);
-            
-            /**while (res != ad) {
+            while (res != num) {
+                  mayor = res>num ? "menor" : "mayor";
+                  System.out.println("El número es " + mayor + " al número dado");
                   System.out.println("Fallaste");
-                  System.out.print("acierta un numero del 0 al 10: ");
-                  Scanner sc = new Scanner(System.in);
-                  int res = sc.nextInt();
-                  }
-            *
-            */
-            
+                  System.out.print("Acierta un numero del 0 al 100: ");
+                  res = sc.nextInt();
+
+            }
+            sc.close();
+            System.out.println("Acertaste");
 
 
 
