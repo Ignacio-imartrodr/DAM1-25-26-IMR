6d8833b58836f7ad9de27f915679182ea5137045
Ignacio imartrodr <ignaciomartinezrodriguez5@gmail.com>
2025-09-26 13:14:09 +0200

    Mejora de Adivina el numero

--- src/AdivinaElNumero.java
+++ src/AdivinaElNumero.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 /** 
-* 
+* Acierta un numero con 10 intentos y pistas de si es mayor o menor
 */
 
 /** 
@@ -11,27 +11,34 @@ public class AdivinaElNumero {
       public static void main(String[] args) throws Exception {
 
             long num;
-            int res;
+            int res = -1;
             String mayor;
+            byte cont;
 
             num = Math.round(Math.random()*100);
-            System.out.print("Acierta un numero del 0 al 100: ");
+            cont = 10;
+            System.out.println(num);
             Scanner sc = new Scanner(System.in);
-            res = sc.nextInt();
-            
-            
-            while (res != num) {
-                  mayor = res>num ? "menor" : "mayor";
-                  System.out.println("El número es " + mayor + " al número dado");
-                  System.out.println("Fallaste");
+
+            while (cont > 0) {
+                  System.out.print("Tienes " +cont+ " intentos. ");
                   System.out.print("Acierta un numero del 0 al 100: ");
                   res = sc.nextInt();
-
+                  if (res != num) {
+                        System.out.println("Fallaste");
+                        mayor = res >num ? "menor" : "mayor";
+                        System.out.println("El número es " + mayor + " al número dado");
+                        --cont;
+                  }else{
+                        System.out.println("Acertaste");
+                        break;
+                  }
             }
             sc.close();
-            System.out.println("Acertaste");
-
-
-
+            if (res != num) {
+                  System.out.println("Has perdido, el número era: " + num);
+            }else{
+                  System.out.println("Has ganado con " + cont + " intentos restantes");
+            }
       }
 }
