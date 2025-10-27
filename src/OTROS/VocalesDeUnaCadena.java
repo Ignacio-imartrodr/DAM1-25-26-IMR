3c79f8724e1a40f174d0de70def787a6bf8e93c3
Ignacio imartrodr <ignaciomartinezrodriguez5@gmail.com>
2025-10-27 20:24:06 +0100

    Cadena con Vocales

--- src/OTROS/VocalesDeUnaCadena.java
+++ src/OTROS/VocalesDeUnaCadena.java
@@ -1,20 +1,33 @@
 package OTROS;
-
+import java.util.Scanner;
 public class VocalesDeUnaCadena {
-    /* Era solo un caracter
     public static boolean contieneVocal(String cad) {
-        char data[] = {'a', 'e', 'i', 'o', 'u'};
-        for (int i = 0; i<=length(cad); ++i){
-            if (cad.charAt(i) != data ){
-                
+        boolean vocal = false;
+        for (int i = 0; i < cad.length(); i++){
+            vocal = switch (cad.charAt(i)) {
+            case 'a', 'e', 'i', 'o', 'u',
+                 'A', 'E', 'I', 'O', 'U',
+                 'á', 'é', 'í', 'ó', 'ú',
+                 'Á', 'É', 'Í', 'Ó', 'Ú',
+                 'ä', 'ë', 'ï', 'ö', 'ü',
+                 'Ä', 'Ë', 'Ï', 'Ö', 'Ü' ->{
+                yield true;}
+            default->{
+                yield false;}
+            };
+            if (vocal) {
+                return vocal;
             }
         }
-
-    
-        return true;
+        return vocal;
     }
     public static void main(String[] args) {
+
+        System.out.print("Dame una cadena de texto para analizar si tiene una vocal: ");
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        sc.close();
+        System.out.print(contieneVocal(str)? "La cadena tiene vocales" : "La cadena NO tiene vocales");
         
     }
-    */
 }
