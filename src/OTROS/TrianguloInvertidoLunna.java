394caa5d9254ad7b57354b270cb45ab6e4963d94
Ignacio imartrodr <ignaciomartinezrodriguez5@gmail.com>
2025-11-12 22:28:54 +0100

    Triangulo inverso

---
+++ src/OTROS/TrianguloInvertidoLunna.java
@@ -0,0 +1,21 @@
+package OTROS;
+/**
+ * @author Lunna
+ */
+public class TrianguloInvertidoLunna {
+    public static void dibujarTriangulo(int n) {
+        for (int i = n; i > 0; i--) {
+            for (int e = n - i; e >= 0; e--) {
+                System.out.print(" ");
+            }
+            for (int t = 1; t <= i; t++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        final int num = 3;
+        dibujarTriangulo(num);
+    }
+}
