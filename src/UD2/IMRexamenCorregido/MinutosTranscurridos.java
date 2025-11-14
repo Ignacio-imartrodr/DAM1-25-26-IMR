411bbd87cd330b52e71e881d63faf29aa12c60c8
Ignacio imartrodr <ignaciomartinezrodriguez5@gmail.com>
2025-11-14 22:49:53 +0100

    Documentación minutos

--- src/UD2/IMRexamenCorregido/MinutosTranscurridos.java
+++ src/UD2/IMRexamenCorregido/MinutosTranscurridos.java
@@ -7,6 +7,9 @@ public class MinutosTranscurridos {
         if (((horaA < 0 || horaA > 23)||(horaB < 0 || horaB > 23))||((minutoA < 0 ||minutoA > 59)||(minutoB < 0 || minutoB > 59))) {
             return -1;
         }
+        //               ^
+        /* Lógica propia |
+        //               V
         int difMin = 0;
         int difHMin = 0;
         if (horaA > horaB) {
@@ -18,17 +21,18 @@ public class MinutosTranscurridos {
         } else if (horaA == horaB){
             difMin = minutoA > minutoB ? 60-((60-minutoA)+ minutoB) : 60-((60-minutoB)+ minutoA);
         }
-        return difHMin + difMin;
-        /*Solución del profe (creo[era algo así, revisalo])
-        return Math.abs((horaA*60 - minutoB)-(horaB*60 - minutoA))
-        */
+        return difHMin + difMin;*/
+        // Solución del profe (inclullendo el if de arriba) |
+        //                                                  V
+        return Math.abs((horaA*60 - minutoB)-(horaB*60 - minutoA));
+        
     }
 
     public static void main(String[] args) {
         final int minutoA = 13;
-        final int horaA = 20;
-        final int minutoB = 10;
-        final int horaB = 10;
+        final int horaA = 10;
+        final int minutoB = 14;
+        final int horaB = 11;
         int diferenciaEnMinutos = minutosTranscurridos(horaA, minutoA, horaB, minutoB);
         System.out.printf(diferenciaEnMinutos != -1 ? "Hay una diferencia de "+ diferenciaEnMinutos +" minutos entre las dos horas" : "Error en la entrada de datos");
     }
