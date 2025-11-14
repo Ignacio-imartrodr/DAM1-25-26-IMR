b732164c20830909084867732c9bfe198b522a85
Ignacio imartrodr <ignaciomartinezrodriguez5@gmail.com>
2025-11-14 12:25:49 +0100

    Examen para a medio resolver

--- src/UD2/IMRexamenResuelto/MinutosTranscurridos.java
+++ src/UD2/IMRexamenResuelto/MinutosTranscurridos.java
@@ -1,7 +1,9 @@
-package OTROS;
-
-public class DiferenciaDeHorasExamen {
-    static int diferenciaDeMinutos(int horaA, int horaB, int minutoA, int minutoB) {
+package UD2.IMRexamenResuelto;
+/**
+ * @author Ignacio MR
+*/
+public class MinutosTranscurridos {
+    static int minutosTranscurridos(int horaA, int minutoA, int horaB, int minutoB) {
         if (((horaA < 0 || horaA > 23)||(horaB < 0 || horaB > 23))||((minutoA < 0 ||minutoA > 59)||(minutoB < 0 || minutoB > 59))) {
             return -1;
         }
@@ -16,8 +18,13 @@ public class DiferenciaDeHorasExamen {
         }
         return difHMin + difMin;
     }
+
     public static void main(String[] args) {
-        int diferenciaEnMinutos = diferenciaDeMinutos(0,22,21,50);
+        final int minutoA = 13;
+        final int horaA = 20;
+        final int minutoB = 10;
+        final int horaB = 10;
+        int diferenciaEnMinutos = minutosTranscurridos(horaA, minutoA, horaB, minutoB);
         System.out.printf(diferenciaEnMinutos != -1 ? "Hay una diferencia de "+ diferenciaEnMinutos +" minutos entre las dos horas" : "Error en la entrada de datos");
     }
 }
