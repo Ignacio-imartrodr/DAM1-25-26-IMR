d27025e1af1a910298b4a9d2dcd6dd5091059d7c
Ignacio imartrodr <ignaciomartinezrodriguez5@gmail.com>
2025-11-19 12:37:33 +0100

    Clase Math

---
+++ src/UD3/ClaseMath.java
@@ -0,0 +1,19 @@
+package UD3;
+
+/**
+ * @author Ignacio MR
+*/
+public class ClaseMath {
+    
+    static double random (double min, double max) {
+        double numRnd = Math.random();
+        numRnd *= max - min;
+        numRnd += min;
+        return numRnd;
+    }
+    public static void main(String[] args) {
+        for (int i = 0; i < 10000; i++) {
+           System.out.println(random(-360,360)); 
+        }
+    }
+}
