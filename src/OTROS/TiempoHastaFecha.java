6f936fd7a7896c4b726f1d40c9ec3951f581d1e8
Ignacio imartrodr <ignaciomartinezrodriguez5@gmail.com>
2025-11-20 14:26:48 +0100

    Diferencia fechas por terminar

---
+++ src/OTROS/TiempoHastaFecha.java
@@ -0,0 +1,27 @@
+package OTROS;
+
+import java.time.LocalDate;
+
+public class TiempoHastaFecha {
+        public static void main(String[] args) {
+        LocalDate hoy = LocalDate.now();
+        System.out.println(hoy);
+
+        int anhoHoy = hoy.getYear();
+        System.out.println(anhoHoy);
+
+        LocalDate inicioVacaciones = LocalDate.of(anhoHoy, 12, 19);
+        System.out.println(inicioVacaciones);
+
+        LocalDate finVacaciones = LocalDate.of(anhoHoy+1, 1, 8);
+        System.out.println(finVacaciones);
+
+        if (hoy.isAfter(inicioVacaciones) && hoy.isBefore(finVacaciones)) {
+            System.out.println("Estamos de vacaciones");
+        } else {
+            System.out.println("Aún no estamos de vacaciones de Navidad");
+            int dias = 0;// TODO Calcular
+            System.out.println("Faltan... " + dias + " dias");
+        }
+    }
+}
