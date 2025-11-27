7271049bf0c8454b62624e3b8ae88390575d4a5a
Ignacio imartrodr <ignaciomartinezrodriguez5@gmail.com>
2025-11-27 12:45:24 +0100

    Dirección en memoria de Arries

---
+++ src/UD3/TresTablas.java
@@ -0,0 +1,15 @@
+package UD3;
+public class TresTablas {
+    public static void main(String[] args) {
+        
+    
+        int[] tablaEnteros = new int[5];
+        double[] tablaDouble = new double[5];
+        boolean[] tablaBoolean = new boolean[5];
+        System.out.println(tablaEnteros);
+        System.out.println(tablaDouble);
+        System.out.println(tablaBoolean);
+    }
+
+
+}
