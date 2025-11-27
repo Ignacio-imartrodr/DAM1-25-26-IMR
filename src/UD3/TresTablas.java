3670540dcad42d05b9f333dc7bf7c040c0e6e12c
Ignacio imartrodr <ignaciomartinezrodriguez5@gmail.com>
2025-11-27 12:48:13 +0100

    Comentarios en Arrays

--- src/UD3/TresTablas.java
+++ src/UD3/TresTablas.java
@@ -2,10 +2,12 @@ package UD3;
 public class TresTablas {
     public static void main(String[] args) {
         
-    
+        //Arrays
         int[] tablaEnteros = new int[5];
         double[] tablaDouble = new double[5];
         boolean[] tablaBoolean = new boolean[5];
+
+        //Direcciones
         System.out.println(tablaEnteros);
         System.out.println(tablaDouble);
         System.out.println(tablaBoolean);
