1b3cc7d637590d43dc0f0e5718cfcc267d21d953
Ignacio imartrodr <ignaciomartinezrodriguez5@gmail.com>
2025-12-02 14:28:49 +0100

    Más Arrays

---
+++ src/UD3/E0505_ArrayPares.java
@@ -0,0 +1,45 @@
+package UD3;
+
+import java.util.Arrays;
+import java.util.Random;
+
+/**
+ *  E0505. Escribe una función que crea y devuelve una tabla ordenada de la longitud especificada rellena con 
+ *  números pares aleatorios en el rango desde 2 hasta el valor de fin inclusive. 
+ * 
+ * @author Ignacio MR
+ */
+
+public class E0505_ArrayPares {
+    static String stringArray(int[] array) {
+        String arrayStr="";
+            for (int i = 0; i < array.length; i++) {
+                if (i == 0) {
+                    arrayStr +='[';
+                } else if (i>0 && i<array.length) {
+                    arrayStr +=',';
+                }
+                arrayStr += array[i];
+                if (i == array.length - 1){
+                    arrayStr +=']';
+                }
+            }
+        return arrayStr;
+    }
+    static int[] rellenaPares(int longitud, int fin){
+        Random rnd = new Random();
+        int random;
+        int[] pares = new int[longitud];
+        for (int i = 0; i < longitud; i++) {
+            do {
+                random = rnd.nextInt(fin - 1) + 2;
+            } while (random%2!=0);
+            pares[i] = random;
+        }
+        Arrays.sort(pares);
+        return pares;
+    }
+    public static void main(String[] args) {
+        System.out.println(stringArray(rellenaPares(5, 20)));
+    }
+}
---
+++ src/UD3/E0506_Primitiva.java
@@ -0,0 +1,47 @@
+package UD3;
+
+import java.util.Arrays;
+import java.util.Random;
+
+/**
+ * E0506_Primitiva. Definir una función que tome como parámetros dos tablas, la primera 
+ * con los 6 números de una apuesta de la primitiva, y la segunda (ordenada) con los 6 
+ * números de la combinación ganadora. La función devolverá el número de aciertos.
+ * 
+ * Además crea un método que devuelve una tabla de números enteros aleatorios entre dos 
+ * números y de una longitud especificada.  
+ * 
+ * @author Ignacio MR
+ */
+
+public class E0506_Primitiva {
+    static int numAciertos(int[] apuesta, int[] ganadora){
+        int aciertos = 0;
+        for (int i = 0; i < ganadora.length; i++) {
+            if (apuesta[i]==ganadora[i]) {
+                aciertos++;
+            }
+        }
+        return aciertos;
+    }
+    static int[] tablaAleatoria(int numInicio, int numFin, int longitud){
+        Random rnd = new Random();
+        int random;
+        int[] arrayRnd = new int[longitud];
+        for (int i = 0; i < arrayRnd.length; i++) {
+            random=rnd.nextInt(numFin-(numInicio-1)) + numInicio;
+            arrayRnd[i]=random;
+        }
+        return arrayRnd;
+    }
+    public static void main(String[] args) {
+        final int longPrimitiva = 6;
+        final int numInicio = 0;
+        final int numFin = 9;
+        int[] ganadora = tablaAleatoria(numInicio, numFin, longPrimitiva);
+        int[] apuesta = ganadora.clone();
+        Arrays.sort(ganadora);
+        System.out.println(numAciertos(apuesta, ganadora));
+    }
+
+}
