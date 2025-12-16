2f1106907c2907eec1bd06e7e512b80a7aa51ada
Ignacio imartrodr <ignaciomartinezrodriguez5@gmail.com>
2025-12-16 12:14:17 +0100

    Moda

---
+++ src/Extras/Moda.java
@@ -0,0 +1,77 @@
+package Extras;
+
+import java.util.Arrays;
+
+public class Moda {
+    static String stringArray(int[] array) {
+        String arrayStr = "[";
+        if (array.length == 0) {
+            arrayStr += ']';
+            return arrayStr;
+        }
+        for (int i = 0; i < array.length; i++) {
+            if (i > 0 && i < array.length) {
+                arrayStr += '|';
+            }
+            arrayStr += array[i];
+            if (i == array.length - 1) {
+                arrayStr += ']';
+            }
+        }
+        return arrayStr;
+    }
+    static int contador(int t[], int clave){
+        int cant = 0;
+        for (int i = 0; i < t.length; i++) {
+            if (t[i]==clave) cant++;
+        }
+        return cant;
+    }
+    static int buscar(int t[], int clave) {
+        for (int i = 0; i < t.length; i++) {
+            if (t[i]==clave) {
+                return i;
+            }
+        }
+        return -1;
+    }
+    static int[] tNumsDif(int t[]){
+        int[] tNumDif = t.clone();
+        int posRep;
+        for (int i = 0; i < tNumDif.length; i++) {
+            posRep = buscar(tNumDif, tNumDif[i]);
+            while (posRep !=i && posRep != -1 && tNumDif[i]==tNumDif[posRep]) {
+                tNumDif[i] = tNumDif[tNumDif.length-1];
+                tNumDif = Arrays.copyOfRange(tNumDif,0, tNumDif.length-1);
+                i--;
+            }
+        }
+        return tNumDif;
+    }
+    static int PosNumMayor(int[] t){
+        int posMayor = 0;
+        for (int i = 0; i < t.length-1; i++) {
+            if(t[i+1] > t[i] && t[i+1] > t[0]) posMayor=i+1;
+            System.out.println("i"+i);
+            System.out.println("pos"+posMayor);
+        }
+        System.out.println(posMayor);
+        boolean SinRepetidos = t.length != tNumsDif(t).length ? false : true; 
+        return SinRepetidos ? posMayor : -1;
+    }
+    static int moda(int[] t){
+        int[] tNumsDif = tNumsDif(t);
+        int[] modas=new int[tNumsDif.length];
+        for (int i = 0; i < modas.length; i++) {
+            modas[i]=contador(t, tNumsDif[i]);
+        }
+        return PosNumMayor(modas) == -1? -1 : tNumsDif[PosNumMayor(modas)];
+    }
+    public static void main(String[] args) {
+        //int t[]={1, 2, 3, 2, 3, 1};//-1
+        //int t[]={1, 2, 1, 2, 3, 1};//1
+        int t[]={1, 2, 1, 2, 2, 3};//2
+        //int t[]={1, 2, 3, 2, 3, 3};//3
+        System.out.println(moda(t)!=-1 ? "El numero moda es: " + moda(t) : "No hay una moda concreta");
+    }
+}
