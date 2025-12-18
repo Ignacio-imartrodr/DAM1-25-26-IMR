d30090ab596a904d654c02da5d27ebb00e513b6b
Ignacio imartrodr <ignaciomartinezrodriguez5@gmail.com>
2025-12-18 09:31:06 +0100

    tRandom completo

--- src/UD3/ClaseAjedrez.java
+++ src/UD3/ClaseAjedrez.java
@@ -1,6 +1,7 @@
 package UD3;
 
 import java.util.Arrays;
+import java.util.Random;
 
 /**
  * @author Ignacio MR
@@ -23,6 +24,7 @@ public class ClaseAjedrez {
         }
         return arrayStr;
     }
+
     static char[][] tableroVacio() {
         char[][] t = new char[8][8];
         for (int i = 0; i < t.length; i++) {
@@ -32,40 +34,44 @@ public class ClaseAjedrez {
         }
         return t;
     }
+
     static void mostrarTableroColoresCasillas(char[][] t) {
         for (int i = 0; i < t.length; i++) {
             for (int j = 0; j < t[i].length; j++) {
                 if ((i + j) % 2 == 0) {
-                    System.out.print((t[i][j]='B') + "  "); // Casilla blanca
+                    System.out.print((t[i][j] = 'B') + "  "); // Casilla blanca
                 } else {
-                    System.out.print((t[i][j]='N') + "  "); // Casilla negra
+                    System.out.print((t[i][j] = 'N') + "  "); // Casilla negra
                 }
             }
             System.out.println();
         }
     }
+
     static void mostrarTablero(char[][] t) {
         for (int i = 0; i < t.length; i++) {
             for (int j = 0; j < t[i].length; j++) {
                 if ((i + j) % 2 == 0) {
-                    System.out.print(t[i][j] + "  "); // Casilla blanca
+                    System.out.print(t[i][j] + "  ");
                 } else {
-                    System.out.print(t[i][j] + "  "); // Casilla negra
+                    System.out.print(t[i][j] + "  ");
                 }
             }
             System.out.println();
         }
     }
-    static char [][] inicializarTablero() {
+
+    static char[][] inicializarTablero() {
         char[][] t = tableroVacio();
         // Colocar piezas negras
-        t[0] = new char[] {'t', 'c', 'a', 'd', 'r', 'a', 'c', 't'};
-        t[1] = new char[] {'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'};
+        t[0] = new char[] { 't', 'c', 'a', 'd', 'r', 'a', 'c', 't' };
+        t[1] = new char[] { 'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p' };
         // Colocar piezas blancas
-        t[6] = new char[] {'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'};
-        t[7] = new char[] {'T', 'C', 'A', 'D', 'R', 'A', 'C', 'T'};
+        t[6] = new char[] { 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P' };
+        t[7] = new char[] { 'T', 'C', 'A', 'D', 'R', 'A', 'C', 'T' };
         return t;
     }
+
     static void mostrarTableroConLeyenda(char[][] t) {
         for (int i = 0; i < t.length; i++) {
             for (int j = 0; j < t[i].length; j++) {
@@ -77,9 +83,9 @@ public class ClaseAjedrez {
             System.out.println();
         }
         System.out.println();
-        System.out.print("     A  B  C  D  E  F  G  H"); // Letras de columnas
-        System.arraycopy(t, 0, t, 0, t.length);
+        System.out.println("     A  B  C  D  E  F  G  H"); // Letras de columnas
     }
+
     static int[] contarPiezasPorFila(char[][] t) {
         int[] piezasPorFila = new int[8];
         for (int i = 0; i < t.length; i++) {
@@ -93,6 +99,7 @@ public class ClaseAjedrez {
         }
         return piezasPorFila;
     }
+
     static int[] contarPiezasPorColumnas(char[][] t) {
         int[] piezasPorColumnas = new int[8];
         for (int j = 0; j < t[0].length; j++) {
@@ -106,33 +113,82 @@ public class ClaseAjedrez {
         }
         return piezasPorColumnas;
     }
-    static int[] posicionAleatoriaVacia(char[][] t){
+
+    static int[] posicionAleatoriaVacia(char[][] t) {
+        Random rnd = new Random();
+        int posRandomF;
+        int posRandomC;
         int[] posVacia = new int[2];
-        for (int i = 0; i < t.length; i++) {
-            for (int j = 0; j < t.length; j++) {
-                if (t[i][j] != '-') {
-                    posVacia[0]=i;
-                    posVacia[1]=j;
-                }
-            }
-        }
+        do {
+            posRandomF = rnd.nextInt(t.length);
+            posRandomC = rnd.nextInt(t.length);
+        } while (t[posRandomF][posRandomC] != '-');
+
+        posVacia[0] = posRandomF;
+        posVacia[1] = posRandomC;
+
         return posVacia;
     }
-    static char[][] tableroAleatorio(){
-        char[] piezasNegras ={'t', 'c', 'a', 'd', 'r', 'p'};
-        char[] piezasBlancas ={'T', 'c', 'a', 'd', 'r', 'p'};
-        char[][] tAleatorio = tableroVacio();
 
+    static char[][] tableroAleatorio() {
+        //char[] piezasNegras = {'t', 'c', 'a', 'd', 'r', 'p'};  //Todo el set de piezas
+        //char[] piezasBlancas = {'T', 'C', 'A', 'D', 'R', 'P'}; //Todo el set de piezas
+        char[] piezasNegras = {'r'}; //Caso del ejercio
+        char[] piezasBlancas = {'R', 'P'}; //Caso del ejercio
+        char[][] tAleatorio = tableroVacio();
+        int[] posRndVacia;
+        for (int i = 0; i < piezasNegras.length; i++) {
+            if(piezasNegras[i] == 'p') {
+                for (int j = 0; j < tAleatorio[2].length; j++) {
+                    posRndVacia=(posicionAleatoriaVacia(tAleatorio));
+                    tAleatorio[posRndVacia[0]][posRndVacia[1]]=piezasNegras[i];
+                }
+            } 
+            else {
+                posRndVacia=(posicionAleatoriaVacia(tAleatorio));
+                tAleatorio[posRndVacia[0]][posRndVacia[1]]=piezasNegras[i];
+            }
+        }
+        for (int i = 0; i < piezasBlancas.length; i++) {
+            if(piezasBlancas[i] ==  'P') {
+                // Caso general para todo el set de piezas
+                /*for (int j = 0; j < tAleatorio[tAleatorio.length-2].length; j++) {
+                    posRndVacia=(posicionAleatoriaVacia(tAleatorio));
+                    tAleatorio[posRndVacia[0]][posRndVacia[1]]=piezasBlancas[i];
+                }*/
+                for (int j = 0; j < 4; j++) {//Caso ejercicio
+                    do {
+                        posRndVacia=(posicionAleatoriaVacia(tAleatorio));
+                    } while (posRndVacia[0]==0 || posRndVacia[0] == tAleatorio.length-1 );
+                    tAleatorio[posRndVacia[0]][posRndVacia[1]]=piezasBlancas[i];
+                }
+            } 
+            else {
+                posRndVacia=(posicionAleatoriaVacia(tAleatorio));
+                tAleatorio[posRndVacia[0]][posRndVacia[1]]=piezasBlancas[i];
+            }
+        }
+        
         return tAleatorio;
     }
+
     public static void main(String[] args) {
-        /*mostrarTableroColoresCasillas(tableroVacio());
+        /*
+        mostrarTableroColoresCasillas(tableroVacio());
         System.out.println();
         mostrarTablero(inicializarTablero());
         System.out.println();
-        mostrarTableroConLeyenda(inicializarTablero());*/
+        mostrarTableroConLeyenda(inicializarTablero());
+         */
         char[][] tablero = inicializarTablero();
-        System.out.println(stringArray(contarPiezasPorFila(tablero)));
-        System.out.println(stringArray(contarPiezasPorColumnas(tablero)));
-    }       
+        char[][] t = tableroAleatorio();
+
+        mostrarTableroConLeyenda(tablero);
+        System.out.println("Piezas por fila: " + stringArray(contarPiezasPorFila(tablero)));
+        System.out.println("Piezas por columna: " + stringArray(contarPiezasPorColumnas(tablero)));
+        System.out.println();
+        mostrarTableroConLeyenda(t);
+        System.out.println("Piezas por fila: " + stringArray(contarPiezasPorFila(t)));
+        System.out.println("Piezas por columna: " + stringArray(contarPiezasPorColumnas(t)));
+    }
 }
