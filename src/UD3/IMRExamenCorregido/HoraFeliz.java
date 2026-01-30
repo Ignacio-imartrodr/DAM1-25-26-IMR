286948ce2c20757d369d051602a27f88f2ba972f
Ignacio imartrodr <ignaciomartinezrodriguez5@gmail.com>
2026-01-30 09:43:10 +0100

    Parche caso límite

--- src/UD3/IMRExamenCorregido/HoraFeliz.java
+++ src/UD3/IMRExamenCorregido/HoraFeliz.java
@@ -18,12 +18,17 @@ public class HoraFeliz {
         if (horaSistema < horaFeliz[0] || (horaSistema==horaFeliz[0] && minutosSistema < horaFeliz[1])){
             System.out.printf("La hora feliz es a las %02d:%02d %n", horaFeliz[0], horaFeliz[1]);
             int minRestantes;
+            int horasRestantes = horaFeliz[0]-horaSistema;
             if (minutosSistema < horaFeliz[1]) {
                 minRestantes = horaFeliz[1]-minutosSistema;
             } else {
-                minRestantes = (60+horaFeliz[1])- minutosSistema;
+                minRestantes = (60+horaFeliz[1])- minutosSistema; 
+                if (minRestantes == 60){
+                    minRestantes = 0;
+                    horasRestantes++;
+                }
             }
-            System.out.printf("Aún faltan %02d horas y %02d minutos", horaFeliz[0]-horaSistema, minRestantes);
+            System.out.printf("Aún faltan %02d horas y %02d minutos", horasRestantes, minRestantes);
         } else if(( (horaSistema == horaFeliz[0]+1 && minutosSistema > horaFeliz[1]) || horaSistema > horaFeliz[0]+1)){
             System.out.printf("La hora feliz ya ha terminado, fué a las %02d:%02d, más suerte la proxima vez!", horaFeliz[0], horaFeliz[1]);
         } else {
