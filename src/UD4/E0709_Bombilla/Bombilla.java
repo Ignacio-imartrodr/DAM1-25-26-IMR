7be9a8637d5527e5b2d7b0d789d87d42936df0d2
Ignacio imartrodr <ignaciomartinezrodriguez5@gmail.com>
2026-02-11 12:44:56 +0100

    Ej Bombilla

---
+++ src/UD4/E0709_Bombilla/Bombilla.java
@@ -0,0 +1,61 @@
+package UD4.E0709_Bombilla;
+
+public class Bombilla {
+    private boolean interruptor;
+    private static boolean general = true;
+
+    Bombilla(){
+        interruptor = false;
+    }
+
+    Bombilla(boolean estado){
+        interruptor = estado;
+    }
+
+    boolean estaEncendido(){
+        return  general && interruptor;
+    }
+
+    boolean getInterruptor(){
+        return  interruptor;
+    }
+
+    void encender(){
+        interruptor = true;
+    }
+
+    void apagar(){
+        interruptor = false;
+    }
+
+    void alternar(){
+        interruptor = !interruptor;
+    }
+
+    static boolean getGeneral(){
+        return  general;
+    } 
+
+    static void encenderGeneral(){
+        general = true;
+    }
+
+    static void apagarGeneral(){
+        general = false;
+    }
+
+    static void alternarGeneral(){
+        general = !general;
+    }
+
+    public static void main(String[] args) {
+        Bombilla b1 = new Bombilla();
+        b1.encender();
+        System.out.println("Está encendida?: " + b1.estaEncendido());
+        System.out.println("Está el interruptor encendido?: " + b1.getInterruptor());
+        Bombilla.alternarGeneral();
+        System.out.println("Apagamos el general");
+        System.out.println("Está encendida?: " + b1.estaEncendido());
+        System.out.println("Está el interruptor encendido?: " + b1.getInterruptor());
+    }
+}
