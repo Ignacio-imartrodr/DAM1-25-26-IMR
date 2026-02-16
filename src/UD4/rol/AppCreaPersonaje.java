0ea8902f5b70ac13f92fe6e8288308b47c3c63e1
Ignacio imartrodr <ignaciomartinezrodriguez5@gmail.com>
2026-02-17 00:58:15 +0100

    Juego Rol "base" terminado

--- src/UD4/rol/AppCreaPersonaje.java
+++ src/UD4/rol/AppCreaPersonaje.java
@@ -14,7 +14,7 @@ import org.json.JSONObject;
  */
 
 public class AppCreaPersonaje {
-    public static Personaje[] personajesDeJson(String ruta){
+    public static Personaje[] getPersonajesJson(String ruta){
         JSONArray personajes = Util.JsonArray(ruta);
         Personaje[] personajesJson = new Personaje[0];
         for (int i = 0; i < personajes.length(); i++) {
@@ -28,41 +28,61 @@ public class AppCreaPersonaje {
         }
         return personajesJson;
     }
-    public static String pedirRutaGuardado(){
-        boolean restart = true;
-        String rutaFichero = "-1";
-        while (restart) {
-            restart = false;
-            System.out.print("¿Quieres guardar en un fichero? (S/n): ");
-            if (Util.escogerOpcion("s", "n")) {
-                System.out.println("OPCIONES: \nJson o Csv");
-                System.out.print("¿Tipo de fichero? (J/c): ");
-                if (Util.escogerOpcion("j", "c")) {
-                    System.out.print("Ruta del fichero: ");
-                    rutaFichero = Util.pedirPorTeclado(false);
-                    if (!rutaFichero.endsWith(".json")) {
-                        System.out.println("La ruta debe contener un fichero con extensión .json");
-                        restart = true;
-                    } else {
-                        restart = true;
-                    }
-                    
-                } else {            
-                    System.out.print("Ruta del fichero: ");
-                    rutaFichero = Util.pedirPorTeclado(false);
-                    if (!rutaFichero.endsWith(".csv")) {
-                        System.out.println("La ruta debe contener un fichero con extensión .csv");
-                        restart = true;
-                    } else {
-                        restart = true;
-                    }
-                }
-            } else {
-                System.out.println("Personaje no guardado.");
+    public static Personaje[] getPersonajesJsonUrl(String ruta){
+        Personaje[] personajesJson = new Personaje[0];
+        String web = "";
+        JSONArray personajes = new JSONArray();
+        try {
+            web = Util.getJson(ruta);
+        } catch (Exception e) {
+            throw new PersonajeException("Error en la URL");
+        }
+        try {
+            JSONObject ArchivoUrl = new JSONObject(web);
+            personajes = Util.JsonArray(ArchivoUrl.getString("Personajes"));
+        } catch (Exception e) {
+            try {
+                personajes = Util.JsonArray(web);
+            } catch (Exception b) {
+                throw new PersonajeException("Error en el formato de la web");
             }
         }
-        return rutaFichero;
+        
+        for (int i = 0; i < personajes.length(); i++) {
+            if (personajes.getJSONObject(i) != null) {
+                JSONObject jsonObject = new JSONObject();
+                jsonObject = personajes.getJSONObject(i);
+                Personaje personaje = new Personaje(jsonObject.getString("nombre"), jsonObject.getString("raza"), jsonObject.getString("fuerza"), jsonObject.getString("agilidad"), jsonObject.getString("constitucion"), jsonObject.getString("nivel"), jsonObject.getString("experiencia"), true);
+                personajesJson = Arrays.copyOf(personajesJson, personajesJson.length + 1);
+                personajesJson[personajesJson.length - 1] = personaje;
+            }
+        }
+        return personajesJson;
     }
+    public static Personaje[] getPersonajesCsv(String ruta){
+        String[] arrayPersonajes = Util.readFileToStringArray(ruta);
+        Personaje[] personajesExtraidos = new Personaje[0];
+        try{
+            personajesExtraidos = new Personaje[arrayPersonajes.length];
+            String[] atributos;
+            int i = 0;
+            int idPers = 1;
+            for (String personaje : arrayPersonajes) {
+                atributos = new String[7];
+                atributos = personaje.split(",");
+                try {
+                    Personaje newPersonaje = new Personaje(atributos[0], atributos[1], atributos[2], atributos[3], atributos[4], atributos[5], atributos[6], true);
+                    personajesExtraidos[i]= newPersonaje;
+                    i++;
+                } catch (Exception e) {
+                    System.out.println("El personaje número " + idPers + "contiene un error.");
+                }
+                idPers++;
+            }
+        } catch (Exception x) {}
+        return personajesExtraidos;
+    }
+    
     public static Personaje[] pedirPersonajes() {
         Personaje[] personajesNuevos = new Personaje[0];
         boolean seguir = true;
@@ -88,7 +108,7 @@ public class AppCreaPersonaje {
     public static void main(String[] args) {
         Personaje[] personajesNuevos = pedirPersonajes();
         for (Personaje personaje : personajesNuevos) {
-            String ruta = pedirRutaGuardado();
+            String ruta = AppCombateSingular.pedirRutaGuardado();
             if (ruta.equals("-1")) {
                 System.out.println("No se guardó el personaje.");
             } else {
