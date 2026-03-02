47132a82997a8751edb141b421814fc17464de6a
Ignacio imartrodr <ignaciomartinezrodriguez5@gmail.com>
2026-03-02 22:30:29 +0100

    ignore

--- src/UD4/rol/Objetos/Equipamiento/EquipamientoTest.java
+++ src/UD4/rol/Objetos/Equipamiento/EquipamientoTest.java
@@ -1,8 +1,8 @@
-package UD4.Rol.Objetos.Entidades;
+package UD4.Rol.Objetos.Equipamiento;
 
 import UD4.Rol.Objetos.Equipamiento.Armadura.*;
+import UD4.Rol.Objetos.Entidades.Personaje;
 import UD4.Rol.Objetos.Equipamiento.Arma.*;
-import UD4.Rol.Objetos.Equipamiento.Equipamiento;
 import UD4.Rol.Utilidades.EquipamientoException;
 
 public class EquipamientoTest {
