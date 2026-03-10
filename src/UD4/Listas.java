b8c849cf2ffba1bcfdaa1f6ad1e5b68342c12c01
Ignacio imartrodr <ignaciomartinezrodriguez5@gmail.com>
2026-03-10 19:16:51 +0100

    Listas

--- src/UD4/Listas.java
+++ src/UD4/Listas.java
@@ -1,13 +1,16 @@
 package UD4;
 
 import java.util.List;
+import java.util.ArrayList;
 
 public class Listas {
     public static void main(String[] args) {
-        List<Listas> lista = new List<Listas>() {
-            
-        };
+        List<String> lista = new ArrayList<>();
+        lista.add(null);
+        lista.add("a");
         System.out.println(lista);
+        for (String string : lista) {
+            System.out.println(string);
+        }
     }
-    
 }
