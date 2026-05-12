a1c65720f7170bf6243ba76c1c29cfe11d8bd45b
Ignacio imartrodr <ignaciomartinezrodriguez5@gmail.com>
2026-05-12 14:31:44 +0200

    Tablero ajedrez

--- src/UD6/Actividad13.java
+++ src/UD6/Actividad13.java
@@ -2,6 +2,7 @@ package UD6;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
@@ -15,10 +16,31 @@ import javafx.stage.Stage;
  *         pulsado y en qué posición se encuentra dentro del GridPane.
  */
 public class Actividad13 extends Application {
-
+    byte[][] mapa = new byte[][]{
+        {0, 1, 0, 1, 0, 1, 0, 1},
+        {1, 0, 1, 0, 1, 0, 1, 0},
+        {0, 1, 0, 1, 0, 1, 0, 1},
+        {1, 0, 1, 0, 1, 0, 1, 0},
+        {0, 1, 0, 1, 0, 1, 0, 1},
+        {1, 0, 1, 0, 1, 0, 1, 0},
+        {0, 1, 0, 1, 0, 1, 0, 1},
+        {1, 0, 1, 0, 1, 0, 1, 0}
+    };
     @Override
     public void start(Stage primaryStage) throws Exception {
         GridPane gP = new GridPane();
+        for (int i = 0; i < mapa.length; i++) {
+            for (int j = 0; j < mapa[i].length; j++) {
+                Label lbl = new Label();
+
+                if (mapa[i][j] == 1) {
+                    lbl.setStyle("-fx-background-color: black;");
+                }
+
+                lbl.setPrefSize(50, 50);
+                gP.add(lbl, i, j);
+            }
+        }
         Scene scene = new Scene(gP);
         primaryStage.setScene(scene);
         primaryStage.show();
